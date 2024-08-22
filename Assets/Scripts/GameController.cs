6526e2a0c504616a3793d7dc820cccbf9e9949fb
LAB5.1 <lab51@fatec.com>
2024-08-21 22:17:22 -0300

    Pastas esquecidas

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -10,21 +10,29 @@ public class GameController : MonoBehaviour
     public GameObject painel;
     public Pergunta[] perguntas;
     public TMP_Text textoPerg;
+    private int indicePerg = 0;
 
     public void Opcao1()
     {
+        if (perguntas[indicePerg].resposta)
+        {
 
+        }
+        
     }
 
     public void Opcao2()
     {
+        if (!perguntas[indicePerg].resposta)
+        {
 
+        }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        textoPerg.text = perguntas[0].pergunta;
+        textoPerg.text = perguntas[indicePerg].pergunta;
     }
 
     // Update is called once per frame
